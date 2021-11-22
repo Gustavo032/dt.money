02b2645b6b33b9d8ff15f873ec9549025ad68463
Gustavo Ramos <gustavoramosrock@gmail.com>
2021-11-21 22:12:41 -0300

    finalizando toda a inserçao

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -17,18 +17,25 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
     const { createTransaction } = useContext(TransactionsContext)
     const [title, setTitle] = useState('');
     const [amount, setAmount] = useState(0.00);
-    const [type, setType] = useState('deposit');
     const [category, setCategory] = useState('');
+    const [type, setType] = useState('deposit');
 
-    function handleCreateNewTransaction(event: FormEvent) {
+    async function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault();
-        
-        createTransaction({
+
+        await createTransaction({
             title, 
             amount,
             category,
             type
         })
+
+        setTitle('')
+        setAmount(0.00)
+        setCategory('')
+        setType('deposit')
+        onRequestClose();
+
     };
 
     return( 
